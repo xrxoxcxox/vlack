98e6430698107895dc56bf72e0fbf5847bae4cbe
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2021-12-25 14:00:12 +0900

    Format existing code

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/jsx-runtime", "plugin:@typescript-eslint/recommended", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'eslint:recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {}
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
 }
