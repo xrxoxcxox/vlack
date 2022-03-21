8a5e48d90eb9739f7960f901aa58a20f099826a2
Keisuke Watanuki <watanuki@ju-zen.com>
2022-03-21 23:32:48 +0900

    Change sorting in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 export default {
   input: 'src/index.ts',
@@ -15,11 +15,11 @@ export default {
     },
   ],
   plugins: [
+    peerDepsExternal(),
     resolve(),
     typescript({
       declarationDir: 'dist',
       exclude: ['src/**/*.stories.tsx'],
     }),
-    peerDepsExternal(),
   ],
 }
