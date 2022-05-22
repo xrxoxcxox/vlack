22e23e78dac0d7a9b26ca4ebf4e6bef8980fa999
Keisuke Watanuki <watanuki@ju-zen.com>
2022-05-23 02:02:42 +0900

    Update Button component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
   babel: async (options) => ({
--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,25 +1,15 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
 
 import { Button } from './Button'
 
 export default {
   title: 'Button',
   component: Button,
-  argTypes: {
-    priority: {
-      table: {
-        disable: true,
-      },
-    },
-  },
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}></Button>
-)
-
-export const Primary = Template.bind({})
-Primary.args = { children: 'Click me', priority: 'primary' }
-
-export const Secondary = Template.bind({})
-Secondary.args = { children: 'Click me', priority: 'secondary' }
+export const Default: ComponentStoryObj<typeof Button> = {
+  args: {
+    children: 'Click me',
+    priority: 'primary',
+  },
+}
