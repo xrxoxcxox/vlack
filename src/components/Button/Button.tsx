d294999bac61dbc17b6ffa0b7625249a10ea2a02
Keisuke Watanuki <watanuki@ju-zen.com>
2022-05-25 07:24:07 +0900

    Update Button style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -27,19 +27,32 @@ const styles = {
     display: inline-block;
     font-size: var(--font-size-m);
     font-weight: 700;
-    line-height: 1.5;
+    line-height: var(--line-height-m);
     padding: 8px 16px;
+    transition: all 100ms ease-in-out;
     &:disabled {
       cursor: not-allowed;
+      opacity: 0.4;
     }
   `,
   primary: css`
     background-color: var(--color-gray-primary);
     color: var(--color-gray-on-primary);
+    @media (hover: hover) and (pointer: fine) {
+      &:hover:not(:disabled) {
+        background-color: var(--color-gray-primary-variant);
+      }
+    }
   `,
   secondary: css`
     background-color: var(--color-gray-background);
     box-shadow: var(--color-gray-secondary) 0 0 0 2px inset;
     color: var(--color-text-primary);
+    @media (hover: hover) and (pointer: fine) {
+      &:hover:not(:disabled) {
+        background-color: var(--color-gray-background-variant);
+        box-shadow: var(--color-gray-secondary-variant) 0 0 0 2px inset;
+      }
+    }
   `,
 }
