782abb7415ae5fefda56769ba3e165b7e612d5a6
Keisuke Watanuki <watanuki@ju-zen.com>
2022-05-29 19:33:29 +0900

    Remove stylelint-order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   customSyntax: '@stylelint/postcss-css-in-js',
-  plugins: ['stylelint-order'],
   rules: {
     'rule-empty-line-before': 'never',
-    'order/properties-alphabetical-order': true,
   },
 }
