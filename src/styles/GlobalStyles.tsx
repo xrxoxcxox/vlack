8754e26007204807d71d0f0e9632df3943dacc4a
Keisuke Watanuki <watanuki@ju-zen.com>
2022-06-04 14:06:41 +0900

    Update GlobalStyles component

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,4 +1,11 @@
-import { Global } from '@emotion/react'
+import { css, Global } from '@emotion/react'
 import { root } from './tokens'
 
-export const GlobalStyles = () => <Global styles={root} />
+const base = css({
+  body: {
+    color: 'var(--color-gray-90)',
+    lineHeight: 'var(--line-height-m)',
+  },
+})
+
+export const GlobalStyles = () => <Global styles={[root, base]} />
