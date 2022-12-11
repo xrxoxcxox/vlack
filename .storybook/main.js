7a90a42ed0fc4431ca1a5fce942e95a95274bfb1
Keisuke Watanuki <watanuki@ju-zen.com>
2022-12-12 04:40:50 +0900

    Use Vite as a Storybook Builder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,31 @@
 const path = require('path')
+const react = require('@vitejs/plugin-react')
 
 module.exports = {
   stories: ['../src'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
   framework: '@storybook/react',
   babel: async (options) => ({
     ...options,
     presets: [...options.presets, '@emotion/babel-preset-css-prop'],
   }),
+  async viteFinal(config) {
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')),
+    )
+    config.plugins.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: '@emotion/react',
+        babel: {
+          presets: ['@emotion/babel-preset-css-prop'],
+        },
+      }),
+    )
+    return config
+  },
 }
